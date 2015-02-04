e79e5048929ecdf7001997eb21e9d3641b60094b
VerlaCBlack <VerlaCBlack611@gmail.com>
2015-02-04 12:16:23 +0530

    Free Trial version

---
+++ dealicious/dealicious/dealicious/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GlobusLib;
+
+namespace dealicious
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+            pg.Test();
+        }
+
+        public void Test()
+        {
+
+            GlobusHttpHelper hp = new GlobusHttpHelper();
+
+            //hp.getHtmlfromUrl("");
+        
+        }
+    }
+}
